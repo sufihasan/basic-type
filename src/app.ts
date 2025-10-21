478cd09e317af1e34d18819cf8358a6647c9bd63
sufihasan <hasan6nh@gmail.com>
2025-10-21 15:29:46 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+// string
+let name: string = 'hasan';
+
+name = 'kamal';
+
+
+//number
+let number: number = 200;
+
+number = 300
+
+
+// boolean
+
+let isActive: boolean = true;
+
+isActive = false;
+
+
+
+
+
+
