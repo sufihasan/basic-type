0eacc5e98259dd3ee032cf18ba6e5b4911e6476c
sufihasan <hasan6nh@gmail.com>
2025-10-21 15:44:56 +0600

    make array with type

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let scrore = [30, 60, 75];
+console.log(scrore[1]);
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,3 @@
-// string
-let name: string = 'hasan';
 
-name = 'kamal';
-
-
-//number
-let number: number = 200;
-
-number = 300
-
-
-// boolean
-
-let isActive: boolean = true;
-
-isActive = false;
-
-
-
-
-
-
+let scrore: number[] = [30, 60, 75];
+console.log(scrore[1]);
