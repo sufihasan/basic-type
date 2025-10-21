7e0ccda57aceeb9ce73b89aa52c7db9d2e5dbac6
sufihasan <hasan6nh@gmail.com>
2025-10-21 16:24:39 +0600

    array and tuples

--- dist/app.js
+++ dist/app.js
@@ -2,4 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let scrore = [30, 60, 75];
 console.log(scrore[1]);
+// alternative 
+let scores = [60, 70, 80];
+let names = ['hasan', 'kamal', 'jamal'];
+console.log(names);
+// tuples
+let student = ['alice', 22, true];
+console.log(student[0]);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 
 let scrore: number[] = [30, 60, 75];
 console.log(scrore[1]);
+
+// alternative 
+let scores: Array<number> = [60, 70, 80];
+
+
+let names: string[] = ['hasan', 'kamal', 'jamal'];
+console.log(names);
+
+// tuples
+
+let student: [string, number, boolean] = ['alice', 22, true];
+console.log(student[0]);
