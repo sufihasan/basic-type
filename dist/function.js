f4f61de5b9e175bd2c4695b81e39e95c7ff0816a
sufihasan <hasan6nh@gmail.com>
2025-10-22 09:51:42 +0600

    make a functon greet that return friend name in ts

---
+++ dist/function.js
@@ -0,0 +1,11 @@
+"use strict";
+//function in ts syntext
+Object.defineProperty(exports, "__esModule", { value: true });
+// function funtion_name (parameter:perameter_type): return_type{
+// function_body
+// }
+function greet(fName) {
+    return `My friend Name is ${fName}`;
+}
+console.log(greet('Kamal'));
+//# sourceMappingURL=function.js.map
---
+++ src/function.ts
@@ -0,0 +1,12 @@
+
+//function in ts syntext
+
+// function funtion_name (parameter:perameter_type): return_type{
+// function_body
+// }
+
+function greet(fName: string): string {
+    return `My friend Name is ${fName}`;
+}
+
+console.log(greet('Kamal'));
